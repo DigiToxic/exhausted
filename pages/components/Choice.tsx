c51378b841e0eb87cbb533c96d4d9a7ffd671e83
Digi | Toxic <DigiToxic55@gmail.com>
2023-07-10 18:53:54 -0400

    official

--- pages/components/Choice.tsx
+++ pages/components/Choice.tsx
@@ -8,78 +8,26 @@ const Choice: NextPage = () => {
   const { theme } = useTheme();
   const imageSrc =
     theme === "dark"
-      ? "/images/page4/Exhausted RoadMap Page Night.png "
+      ? "/images/page4/Exhausted RoadMap Page Night.png"
       : "/images/page4/Exhausted RoadMap Page Day.png";
+
+  const roadmap =
+    theme === "dark"
+      ? "/images/page4/Exhausted RoadMap Sign Night.png"
+      : "/images/page4/Exhausted RoadMap Sign Day.png";
 
   return (
     <div>
       <Head1 />
-      <div className="flex justify-center items-center">
-        <div className="z-20 absolute pt-72 flex justify-center items-center max-[1335px]:pt-[360px] max-[1000px]:hidden">
-          <div className="w-[51%] bg-black bg-opacity-50 p-2 rounded-3xl">
-            <p className="text-white text-sm text-center max-[1335px]:text-xs">
-              We know that you are here because you are craving to change your
-              life for better, that you are tired of losing money, that you need
-              guidance of direction of which to go. In We Are Exhausted, that is
-              exactly our aim as well as long term mission that we will and are
-              capable to execute. For more than one year, we have been working
-              on the project to provide our community of their best interest, a
-              platform and high level software that includes A to Z step by step
-              blueprints in different business niches led by our experts. Not
-              only that, plenty of resources, networks, webinars, QnAs, in
-              person mastermind events will also be provided; a whole literal
-              facility and unfair edges to each of your own wealth creation.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-start">
-        <div className="z-20 absolute ml-10 flex justify-start box-text max-[1000px]:hidden">
-          <div className="w-[70%] bg-black bg-opacity-50 p-2 rounded-xl rounded-tr-[150px] max-[1285px]:w-[60%]">
-            <p className="text-bold text-white text-sm">Phase 1 :</p>
-            <li className="text-white text-sm">Pre mint utilities</li>
-            <li className="text-white text-sm">Community building</li>
-            <li className="text-white text-sm">
-              Collaborations & Exhausted assets launch
-            </li>
-            <li className="text-white text-sm">Begin the mint</li>
-
-            <p className="text-bold text-white text-sm">Phase 2 :</p>
-            <li className="text-white text-sm">
-              Launch all of the utilities after day 1 of mint
-            </li>
-            <li className="text-white text-sm">Art reveal</li>
-            <li className="text-white text-sm">Partnerships & mergers talks</li>
-
-            <p className="text-bold text-white text-sm">Phase 3 :</p>
-            <li className="text-white text-sm">
-              In real life events twice a year
-            </li>
-            <li className="text-white text-sm">
-              Launch the most powerful SaaS in web3 accessible to our holders
-            </li>
-            <li className="text-white text-sm">
-              Continue growing community & proper branding
-            </li>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-end">
-        <div className="z-20 absolute pt-[470px] mr-8 flex justify-end max-[1360px]:pt-[410px] max-[1000px]:hidden">
-          <div className="w-[35%] p-3 bg-black bg-opacity-50 rounded-3xl">
-            <p className="text-white text-center">
-              While we are confident to guarantee at least a 10x return of
-              investment by taking advantages on our utilities alone, we are
-              aiming to be the first if not the most innovative project bringing
-              massive changes to the industry, setting new standards.
-            </p>
-          </div>
-        </div>
-      </div>
 
       <img className="h-screen w-screen z-0" src={imageSrc} alt="" />
+      <Link href="Roadmap">
+        <img
+          className="absolute w-[472px] h-[300px] top-[180px] left-[530px]"
+          src={roadmap}
+          alt=""
+        />
+      </Link>
       <img
         className="absolute w-[9%] h-[18%] bottom-5 right-[45.2%]"
         src="/images/page4/Exhausted RoadMap Car.gif"
@@ -92,13 +40,11 @@ const Choice: NextPage = () => {
           alt=""
         />
       </Link>
-      {/* <Link href="Utility" className="cursor-pointer">
         <img
-          className="absolute w-[16.1%] h-[20%] left-[2.7%] top-[15.5%]"
+          className="absolute w-[16.1%] h-[20%] left-[2.7%] top-[15.5%] cursor-not-allowed"
           src="/images/page4/Exhausted RoadMap Signs Vault.png"
           alt=""
         />
-      </Link>*/}
     </div>
   );
 };
---
+++ pages/components/Roadmap.tsx
@@ -0,0 +1,164 @@
+import React from "react";
+import Head1 from "./Triple Head/Header1";
+
+function Roadmap() {
+  return (
+    <div className="h-screen overflow-y-scroll sticky">
+      <Head1 />
+      <img
+        className="w-[1150px] h-[800px] absolute -ml-32 -mt-10 -z-10"
+        src="/images/Lines.png"
+        alt=""
+      />
+
+      <div className="flex flex-col justify-center items-center mt-32">
+        <p className="text-white text-2xl font-bold mb-10 text-center">
+          Watch this first before scrolling any further!
+        </p>
+
+        <div className="bg-gray-500 w-[590px] h-[315px] rounded-3xl border-2 flex justify-center items-center max-[700px]:w-[540px] max-[600px]:w-[490px] max-[540px]:w-[430px] max-[485px]:w-[380px] max-[450px]:w-[330px]">
+          <p className="underline text-white text-2xl rotate-[24deg]">
+            Video coming soon!
+          </p>
+        </div>
+        {/* <iframe
+          className="rounded-3xl border-2 border-white max-[700px]:w-[540px] max-[600px]:w-[490px] max-[540px]:w-[430px] max-[485px]:w-[380px] max-[450px]:w-[330px]"
+          width="590"
+          height="315"
+          src="https://www.youtube.com/embed/_u0gD1e59qE"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe> */}
+        <div className="w-1/2 mt-12 max-[1250px]:w-3/4">
+          <p className="text-white text-center">
+            Huff... <br />
+            <br />
+            We know that you are here because you are{" "}
+            <span className="font-bold">
+              craving to change your life for the better
+            </span>
+            , <br />
+            <br />
+            <br /> that you are{" "}
+            <span className="font-bold">tired of losing money</span>,<br />
+            <br />
+            <br /> that you{" "}
+            <span className="font-bold">
+              need guidance of direction of which way to go
+            </span>
+            ,<br />
+            <br />
+            <br /> or you are enough of{" "}
+            <span className="font-bold">
+              cash grabs of scams in the space
+            </span>. <br />
+            <br />
+            <br />
+            In <span className="italic">We Are Exhausted</span>,<br />
+            <br />
+            <br /> that is exactly our <span className="font-bold">aim</span> as
+            well as <span className="font-bold">long term mission</span> that we
+            will and are capable to execute. <br />
+            <br />
+            <br /> For <span className="text-red-500">more than one year</span>,
+            we have been working on the project to provide our community of
+            their best interest, a platform and high level software that
+            includes A to Z step by step blueprints in different business niches
+            led by our experts. <br />
+            <br />
+            <br /> Not only that, plenty of ressources, networks, webinars,
+            QnAs, in person mastermind events will also be provided; a whole
+            literal facility and unfair edges to each of your own wealth
+            creation.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center text-center mt-32">
+        <p className="text-white font-bold text-3xl underline">
+          Exhausted Roadmap
+        </p>
+      </div>
+
+      <div className="absolute overflow-hidden h-[1040px] -z-10 max-[830px]:hidden">
+        <img
+          className="mt-[300px] opacity-70"
+          src="/images/diagram.png"
+          alt=""
+        />
+      </div>
+
+      <div className="flex justify-center items-center mt-16 mb-32">
+        <div className="h-[650px] border-l-2 border-[#183C7A] -ml-16 mr-10 max-[830px]:hidden">
+          <div className="flex flex-col justify-between h-[650px] -ml-[15px]">
+            <div className="flex items-center">
+              <div className="border-[15px] border-[#183C7A] rounded-full mt-10"></div>
+              <span className="mt-9 ml-4 text-2xl font-bold text-[#183C7A]">
+                &lArr;
+              </span>
+            </div>
+            <div className="flex items-center">
+              <div className="border-[15px] border-[#183C7A] rounded-full mt-10"></div>
+              <span className="mt-9 ml-4 text-2xl font-bold text-[#183C7A]">
+                &lArr;
+              </span>
+            </div>
+            <div className="flex items-center">
+              <div className="border-[15px] border-[#183C7A] rounded-full mb-10"></div>
+              <span className="mb-9 ml-4 text-2xl font-bold text-[#183C7A]">
+                &lArr;
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between items-center text-center w-[600px]">
+          <div className="flex flex-col items-center mb-24 w-5/6">
+            <p className="text-white font-bold text-2xl mb-4">Phase 1 :</p>
+            <li className="text-white text-lg">Pre mint utilities</li>
+            <li className="text-white text-lg">Community building</li>
+            <li className="text-white text-lg">
+              Collaboration & exhausted assets launch
+            </li>
+            <li className="text-white text-lg">Begin the mint</li>
+          </div>
+          <div className="flex flex-col items-center mb-24 w-5/6">
+            <p className="text-white font-bold text-2xl mb-3">Phase 2 :</p>
+            <li className="text-white text-lg">
+              Launch all of the utilities after day 1 of mint
+            </li>
+            <li className="text-white text-lg">Art reveal</li>
+            <li className="text-white text-lg">Partnerships & merger talks</li>
+            <li className="text-white text-lg">
+              In real life events twice a year in Greece and Spain
+            </li>
+          </div>
+          <div className="flex flex-col items-center w-5/6">
+            <p className="text-white font-bold text-2xl mb-4">Phase 3 :</p>
+            <li className="text-white text-lg">
+              Launch the most powerful SaaS tool in web3 accessible to our
+              holders
+            </li>
+            <li className="text-white text-lg">
+              Continue growing community and proper branding (IP)
+            </li>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center max-[830px]:mb-14">
+        <div className="w-3/4">
+          <p className="text-white text-center text-lg">
+            While we are condifent to guaranteed at least a 10x return of
+            investment by taking advantages on our utilities alone, we are
+            aiming to be the first if not the most innovative project bringing
+            massive changed to the industry, setting new standards.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Roadmap;
